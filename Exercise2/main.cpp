129bc283d929829db65fa902ab89b48747fa441f
Leonardo Sala <s310598@studenti.polito.it>
2025-03-15 14:38:35 +0100

    countlines_infile function added

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,4 +1,36 @@
-int main()
-{
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+int countlines_infile(string filename) {
+	ifstream ifs(filename);
+	if (ifs.fail()) {
+		cerr << "Unable to open file" << endl;
+		return 1;
+	}
+	
+	int num_lines = 0;
+	string line;
+	while (getline(ifs,line)){
+		++num_lines;
+	}
+	
+	return num_lines;
+}
+
+int main() {
+	string ifilename = "data.txt";
+	ifstream ifs(ifilename);
+	if (ifs.fail()) {
+		cerr << "Unable to open file" << endl;
+		return 1;
+	}
+	
+	int num_lines = countlines_infile(ifilename);
+	cout << num_lines << endl;
+	
     return 0;
 }
