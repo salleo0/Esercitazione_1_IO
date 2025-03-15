be37eef020a18ff5708c70fe38493a5aa9f387de
Leonardo Sala <s310598@studenti.polito.it>
2025-03-15 12:48:24 +0100

    corrected errors in the first part of exercise

--- Exercise1/main.cpp
+++ Exercise1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
 using namespace std;
 
 int main()
@@ -9,27 +10,24 @@ int main()
 	int sum_of_squares = 0;
 	cout << "How many numbers do you want to read from the file? ";
 	cin >> num;
-	
 	string filename = "data.txt";
 	ifstream ifs(filename);
 	
 	if (!ifs.is_open()){
-		cout << "Error: could not open the file\n";
+		cerr << "Error: could not open the file\n";
 		return 1;
 	}
 	
 	string line;
-	getline(filename, line); 		/* discard header line */
+	getline(ifs, line); /* discard header line */
 	int num_in_line;
-	
-	for (int i = 1; i<=num; i++){
-		getline(filename, line);
-		if (line != "\n") {
-			getline(filename, num_in_line);
-			sum_of_squares += num_in_line*num_in_line;
+	int i = 1;
+	while (!ifs.eof() && i<=num) {
+		ifs >> num_in_line;
+		sum_of_squares += num_in_line*num_in_line;
+		i++;
 	}
-	
-	cout << "Sum of the first " << num << "numbers squared: " << sum_of_squares;
+	cout << "Sum of the first " << num << " numbers squared: " << sum_of_squares << endl;
 	
     return 0;
 }
